73c4b69843acf4d17a6f21cb0f1a8bbdbeff92c8
alex <chepiga@ukr.net>
2016-11-15 15:45:06 +0200

    FP2

--- src/main/java/ua/edu/khibs/restaurantAlex/model/Dishes.java
+++ src/main/java/ua/edu/khibs/restaurantAlex/model/Dishes.java
@@ -23,9 +23,7 @@ public class Dishes {
         this.name = name;
     }
 
-    public int getWeight() {
-        return weight;
-    }
+    public int getWeight() { return weight; }
 
     public void setWeight(int weight) {
         this.weight = weight;
